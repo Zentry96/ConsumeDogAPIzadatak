ea3b5930b9a3ddae5c73f58a2c2d12587dcd6bdc
Zentry96 <45236139+Zentry96@users.noreply.github.com>
2024-12-16 16:21:45 +0100

    Creating service and registering it in program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using ConsumeDogAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient<DogApiService>();
 
 var app = builder.Build();
 
---
+++ Services/DogApiService.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsumeDogAPI.Services;
+
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public class DogApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public DogApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<string>> GetTerrierSubBreed()
+    {
+        var response = await _httpClient.GetAsync("https://dog.ceo/api/breed/terrier/list");
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        var data = JsonSerializer.Deserialize<DogApiResponse>(jsonResponse);
+
+        return data?.subBreed ?? new List<string>();
+    }
+
+    private class DogApiResponse
+    {
+        public List<string> subBreed { get; set; }
+        public string Status { get; set; }
+    }
+}
