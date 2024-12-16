49fa4d846dee45e66d04eb82e6d1da634f64a8ef
Zentry96 <45236139+Zentry96@users.noreply.github.com>
2024-12-16 16:13:27 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumeDogAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
